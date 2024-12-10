8ebf692213639b87a551f5ab2043789f41bb8ab8
s6601012610016 <s6601012610016@email.kmutnb.ac.th>
2024-12-11 02:05:02 +0700

    Update settings.py

--- w2/settings.py
+++ w2/settings.py
@@ -15,6 +15,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+PORT = os.environ.get('PORT', 8000)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
